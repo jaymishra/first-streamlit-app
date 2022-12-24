0c8a5d4bf966478a0431dd1595b3849fa5d23ddc
Jay Mishra <mrityunjaym@gmail.com>
2022-12-23 21:41:29 -0500

    fix the name fruit submission
    fix the name fruit submission

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,16 +117,16 @@ if streamlit.button('Get fruit load list'):
     
  
 
-## Allow the end user to add fruit to list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        my_cur.execute("insert into fruit_load_list values ('" + new_fruit  + "')")
         return "Thanks for adding" + new_fruit
     
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
-if streamlit.button('Add Fruit to the lsit'):
+if streamlit.button('Add Fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = get_fruityvice_data(add_my_fruit)
+    streamlit.write('Thanks for adding', add_my_fruit)
     streamlit.text(back_from_function)
 
         
