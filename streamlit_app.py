9d5271bfe35a42d1023e36e123114b02f2565bdb
Jay Mishra <mrityunjaym@gmail.com>
2022-12-23 22:07:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,7 +104,7 @@ except URLError as e:
 #############. Move the Fruit Load List Query and Load into a Button Action
 
 
-streamlit.header("Fruit List contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
