626b401c266a95b14f38fee529f10bf0112bc41a
Jay Mishra <mrityunjaym@gmail.com>
2022-12-23 20:35:55 -0500

    update user field

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,5 +51,5 @@ streamlit.header("Fruit List contains:")
 streamlit.dataframe(my_data_rows)
 
 ## Allow end user to input fruits
-add_my_fruit = streamlit.text_input("What fruit would you like to add?" + fruit_Add)
-streamlit.write('The user wants to add ', fruit_add)
+add_my_fruit = streamlit.text_input("What fruit would you like to add?")
+streamlit.write('Thanks for adding', add_my_fruit)
